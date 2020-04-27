7abbeef791684fd72af605f55180d6f078243bab
Denis <rybakoff32@yandex.ru>
2020-04-27 19:29:40 +0300

    This is first our commit

---
+++ StepikJS/script.js
@@ -0,0 +1,17 @@
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+console.log(arr[0]);
+
+alert(arr[2]);
+
+
+let answer = prompt("Do you want enable cookie?", "");
+alert("Your answer is" + ' "'+answer+'" ');
+console.log(typeof(answer))
+
+let incr = 10,
+    dncr = 15;
+
+incr++;
+dncr--;
+
+alert(incr+" "+ dncr);
